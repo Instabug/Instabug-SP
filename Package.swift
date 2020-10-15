c9dac037b709c50daea8ff6e097bc87b9baae0d8
Yousef Hamza <jo.adam.93@gmail.com>
2020-10-15 14:40:32 +0200

    Add package swift and podspec

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Instabug",
+    products: [
+        .library(
+            name: "Instabug",
+            targets: ["Instabug"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "Instabug",
+            url: "https://github.com/Instabug/Instabug-SP/releases/download/master/Instabug.xcframework.zip",
+            checksum: "665f00109192690a4f633c39152e3dbe6d971672f496fb940c320d8d6e7e294a")
+    ]
+)
