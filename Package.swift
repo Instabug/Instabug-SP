5ec6f2fdbac2e402b80710e66736e12b6ae54c71
Yousef Hamza <jo.adam.93@gmail.com>
2020-10-15 14:44:39 +0200

    Use correct path

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/master/Instabug.xcframework.zip",
+            url: "https://github.com/Instabug/Instabug-SP/releases/download/10.0.1/Instabug.xcframework.zip",
             checksum: "665f00109192690a4f633c39152e3dbe6d971672f496fb940c320d8d6e7e294a")
     ]
 )
