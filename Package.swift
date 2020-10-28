d4888a28da437ca4d705b94c32925effd5a19f46
yousefhamza <jo.adam.93@gmail.com>
2020-10-28 14:18:15 +0200

    Prepare 10.0.2 XCFramework release

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://srv-file9.gofile.io/downloadStore/srv-store1/PNpauO/Instabug.xcframework.zip",
+            url: "https://ios-releases.instabug.com/release/xcframework/10.0.2/archive.zip",
             checksum: "d0e6a915fd35a7f1a50b0edd0d32a12946073e677928d562f61b462ae3be901c")
     ]
 )
