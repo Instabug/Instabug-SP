f3b91b683bf1092d63c509e6e597512cc0e5c594
Distiller <distiller@Distillers-Mac-2.local>
2021-01-21 06:25:12 +0000

    v10.1.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://ios-releases.instabug.com/release/10.1.1/archive.zip",
-            checksum: "04b283f5106543855d3f3781ffd3ff2d3e4e9450650fe0cf56ada90eb5135f4c")
+            url: "https://ios-releases.instabug.com/release/10.1.2/archive.zip",
+            checksum: "c25293b83c0938a03e9e4fd693e8f962822e51b507f86256e6f3dd64607ab52f")
     ]
 )
