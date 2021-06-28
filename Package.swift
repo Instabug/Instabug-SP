762835da7e76c485c8b367b8357c6e4393400132
Distiller <distiller@static.38.39.188.166.cyberlynk.net>
2021-06-28 12:49:18 +0000

    v10.7.4

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://ios-releases.instabug.com/release/10.7.3/archive.zip",
-            checksum: "e9935ee0bc6e350e7c41aebe3ed9557f1ad6027d087521e0d47bbc6d83e5be65")
+            url: "https://ios-releases.instabug.com/release/10.7.4/archive.zip",
+            checksum: "7d332f8b953d8635fb034e011de7d28e6bef0deb98123fc9e4bdf5789dbec16d")
     ]
 )
