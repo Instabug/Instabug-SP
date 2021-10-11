fbcd7f5526c3e5a1a375e3f8e5947dbe6a30f059
Yousef Hamza <jo.adam.93@gmail.com>
2021-10-11 17:32:00 +0200

    Update package checksum

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "Instabug",
             url: "https://ios-releases.instabug.com/release/Instabug/10.9.3/archive.zip",
-            checksum: "1151f47fe969df7c43429c938cd2b7661c9b775543180d2b931cea6dc59a0bbb")
+            checksum: "d8557ca5e3ce264e1622573d254fda99fe0886ae87eb3c76a1587ee42a26962d")
     ]
 )
