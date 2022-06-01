ba29774055982ecca281855b7db4bfa48165f8ba
Yousef Hamza <jo.adam.93@gmail.com>
2022-06-01 16:32:04 +0200

    Release v11.0.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://ios-releases.instabug.com/custom/DoorDash/11.0.1/Instabug/archive.zip",
-            checksum: "e82737e88e2dbdb99aadf7d10999a0012760336c0d8c60ab8ace1b7d80f0c625")
+            url: "https://ios-releases.instabug.com/custom/DoorDash-multiple-url-sessions/11.0.1/Instabug/archive.zip",
+            checksum: "cabefedf966ff271942393753e321c5bbeea0ad68c73043978a6e68f3ae86f8c")
     ]
 )
