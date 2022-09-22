bb90de0356fa98cba4a33c8df2746372dfa7bd71
Instabug-iOS-CI <github-ci+ios@instabug.com>
2022-09-22 12:53:55 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://ios-releases.instabug.com/release/Instabug/11.3.0/archive.zip",
-            checksum: "1ceb341eba79208d8fa381716399b2800cf20f247edeb07fd4b78467da902ce4")
+            url: "https://ios-releases.instabug.com/custom_spm/gojek/11.3.0/Instabug/archive.zip",
+            checksum: "ab889f7680415a21393484912054ef8de5635da7df4720383b5c441666352a8b")
     ]
 )
