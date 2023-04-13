413deb5fbd4ec3ed32be74ca75f504df810da813
Instabug-iOS-CI <github-ci+ios@instabug.com>
2023-04-13 13:22:47 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .binaryTarget(
             name: "Instabug",
             url: "https://ios-releases.instabug.com/custom_spm/doordash/11.10.0/Instabug/archive.zip",
-            checksum: "172e4816f5dac19ff8753ab0cb4fb22f2fb4a4c74b4ec562b5a1a30caa0475ff")
+            checksum: "7cc19cc91022ce8c7b526339cb55a80b7244086b14b60cca592efbf360d97235")
     ]
 )
