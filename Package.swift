2b23ff94d7f533a7244fd64e13333be50d42d265
Instabug-iOS-CI <github-ci+ios@instabug.com>
2025-02-14 13:18:06 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/14.2.0/Instabug-XCFramework.zip",
-            checksum: "33ecf9583435b9c9a5b3854d31fc3e00e0152299bdab07ebf6e5b61640b2dd6a")
+            url: "https://ios-releases.instabug.com/custom_spm/fix-drop-session-on-disable/14.2.0.1/Instabug/archive.zip",
+            checksum: "62d329dae429dcb94299f48d55baa35d29eff64cdb97650873816d36131c919e")
     ]
 )
