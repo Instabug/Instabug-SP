39b33d5a8c41006ee0478d6163fe4843ee25e156
Ahmed Salah <asalah417@gmail.com>
2025-02-16 14:26:23 +0200

    update package file

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/14.2.0/Instabug-XCFramework.zip",
-            checksum: "33ecf9583435b9c9a5b3854d31fc3e00e0152299bdab07ebf6e5b61640b2dd6a")
+            url: "https://ios-releases.instabug.com/custom/pinterest-beta/13.3.1/Instabug/archive.zip",
+            checksum: "ace0839a9d4db942ae64a89aca836f2e305311bd0e84846c8d5dc1fd1f2ab0c2")
     ]
 )
