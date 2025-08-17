b1510ddf602a9d520bc622dd714c9bd20c364a99
Instabug-iOS-CI <github-ci+ios@instabug.com>
2025-08-17 14:11:23 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/16.0.2/Instabug-XCFramework.zip",
-            checksum: "1839835fba2aff04836a65aacfa653c761480b8d92efe7094884f7d61eaa8d42")
+            url: "https://ios-releases.instabug.com/custom_spm/rebase-bdr-therma/16.0.2/Instabug/archive.zip",
+            checksum: "5c3c7eeb0c0e0bb16b96bb73869b4dc8e90fe4ce020fbd5b9d9a6c388467ee28")
     ]
 )
