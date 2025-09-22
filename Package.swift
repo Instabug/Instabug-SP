f5507256700db8208665ad1b2e215f69b171d4b7
Instabug-iOS-CI <github-ci+ios@instabug.com>
2025-09-22 10:39:24 +0000

    update package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/16.0.3/Instabug-XCFramework.zip",
-            checksum: "24abcbd2607b17dc56797972987ef6657105efcbb1f9b0fcb8361eb87ac3f233")
+            url: "https://ios-releases.instabug.com/custom_spm/optimize-app-hangs-base/16.0.3/Instabug/archive.zip",
+            checksum: "872d177f3ead95fda8f95ef2080df82d249c43653d515eae9f39219acd75bdd1")
     ]
 )
